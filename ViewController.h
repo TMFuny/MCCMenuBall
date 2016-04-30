1aededdccb436b7ef34986075aa9e8fdcf759af1
MrChens <chenshilongfly@sina.com>
2016-04-30 12:01:23 +0800

    delete some note
    delete some note

--- ViewController.h
+++ ViewController.h
@@ -10,8 +10,6 @@
 
 @interface ViewController : UIViewController
 
-//This demo is for the float menu with Dynamics.
-//
-//https://developer.apple.com/library/ios/samplecode/DynamicsCatalog/Introduction/Intro.html https://onevcat.com/2013/06/uikit-dynamics-started/
+
 @end
 
